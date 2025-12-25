68c029a523cf5cda7094eae9e23594f2bbaafae7
etal <kou_myl0922@icloud.com>
2025-12-25 17:55:57 +0900

    feat(taskrepo):updateメソッド実装

--- internal/infra/repository/task/store.go
+++ internal/infra/repository/task/store.go
@@ -28,3 +28,49 @@ INSERT INTO task (
 	}
 	return nil
 }
+func (r *Repository) Update(ctx context.Context, t *dtask.Task) error {
+	const q = `
+UPDATE task
+SET
+  title = :title,
+  description = :description,
+  status = :status,
+  due_date = :due_date,
+  updated_at = :updated_at,
+  version = :next_version
+WHERE
+  id = :id
+  AND version = :version;
+` //versionの一致まで確かめる。楽観ロック
+
+	rec := EntityToRecord(t)
+	params := map[string]any{
+		"id":           rec.ID,
+		"title":        rec.Title,
+		"description":  rec.Description,
+		"status":       rec.Status,
+		"due_date":     rec.DueDate,
+		"updated_at":   rec.Updated,
+		"version":      rec.Version,
+		"next_version": rec.Version + 1,
+	}
+	//帰納的に+1を圧合うためのparams。+1の責務をdomainに寄せるならばrecをsqlに直接代入可能
+	//+1の責務をここで持ってることによりdomain側のversionは更新されない。ゆえに今の設計やと更新した後にリロード必須。
+
+	res, err := r.q.NamedExecContext(ctx, q, params)
+	if err != nil {
+		return fmt.Errorf("taskrepo update execute: %w", err)
+	}
+
+	ra, err := res.RowsAffected()
+	//RowsAffectedは何行更新したか返す。insertもupdateも一行。これが0ならば条件不一致。楽観ロック。
+	//いや違うわ。0の時は楽観ロックor存在しない。
+	// TODO:だから存在しない場合はErrNotFoundに分類してもいい。
+	if err != nil {
+		return fmt.Errorf("taskrepo update rowsaffected: %w", err)
+	}
+	if ra == 0 {
+		return dtask.ErrConflict
+	}
+	return nil
+}
