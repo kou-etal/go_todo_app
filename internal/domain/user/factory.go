af219c08f4abc68c70d4717665b7f9357a463a48
etal <kou_myl0922@icloud.com>
2026-01-03 16:57:49 +0900

    feat(domainuser):

---
+++ internal/domain/user/factory.go
@@ -0,0 +1,49 @@
+package user
+
+import (
+	"time"
+)
+
+func NewUser(
+	email UserEmail,
+	password UserPassword,
+	userName UserName,
+	now time.Time,
+) *User {
+	n := normalizeTime(now)
+
+	return &User{
+		id:              NewUserID(), //IDが欲しいのはdomain層の都合
+		email:           email,
+		password:        password,
+		userName:        userName,
+		emailVerifiedAt: nil, //ここを変えれるのはverifyemailメソッドだけ。これを適当に変えさせないためにカプセル
+		createdAt:       n,
+		updatedAt:       n,
+		version:         1,
+	}
+}
+
+// これは復元用。repoで使う
+// domainはDBが持ってる値を信用する
+func ReconstructUser(
+	id UserID,
+	email UserEmail,
+	password UserPassword,
+	userName UserName,
+	emailVerifiedAt *time.Time,
+	createdAt time.Time,
+	updatedAt time.Time,
+	version uint64,
+) *User {
+	return &User{
+		id:              id,
+		email:           email,
+		password:        password,
+		emailVerifiedAt: emailVerifiedAt,
+		userName:        userName,
+		createdAt:       createdAt,
+		updatedAt:       updatedAt,
+		version:         version,
+	}
+}
