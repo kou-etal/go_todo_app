6cd0a23cf126d93db3b634fb6773bcef50b1742f
etal <kou_myl0922@icloud.com>
2026-04-08 16:32:26 +0900

    fix:rewriteのハードコード解消

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from "next";
 
+//ローカルではlocalhost:18000、Vercelでは環境変数でURTを設定。
+//これ便利記法
+const apiUrl = process.env.API_URL ?? "http://localhost:18000";
+
 const nextConfig: NextConfig = {
   async rewrites() {//rewritesはpromise返す。
     return [
       {
-        //componentsでfetch(/api/:path*)した場合http://localhost:18000/:path*にする。
-        //TODO:ここはハードコードしない。
+        //componentsでfetch(/api/:path*)した場合API_URLの値/:path*にする。
         source: "/api/:path*",
-        destination: "http://localhost:18000/:path*",
+        destination: `${apiUrl}/:path*`,
       },
     ];
   },
